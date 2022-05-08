4aadd072105967a2d36eb4c279b25aaa3c4091d9
MJurayev <jurayevmansurbek667@gmail.com>
2022-05-08 17:09:24 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,135 @@
+// console.log("Hello World");
+
+// var name123 = "John Do"
+
+// var myMaxAge = 21 // camelCase 
+// var userNameMaxLength // camelcase
+
+// var MyMaxAge // PascalCase
+// class MyMaxAge {
+// }
+
+// var my_max_age  // snake_case
+
+// my - max - age // kebab-case
+
+// console.log(name123)
+
+// asdfgdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddas
+
+/*asd
+asda
+asda
+asdasd
+*/
+
+// function getBalance() {
+//
+// }
+
+// function getTransactionId() { }
+
+// myAge = 12;r age = 21;
+
+// age = 23
+// myPreviousAge = 23;
+// var sentence
+
+// sentence = 'Amir Temur bobomiz  \'BIzning qudratimizga shubha qilsangiz biz qurgan imoratlarga boqing\' - degan edi ';
+
+// console.log(sentence)
+
+// sentence = "Gap tugadi"
+
+// console.log(sentence)
+
+
+// Numbers
+
+// var age = 21;
+
+// age = 23
+
+// console.log(age + age)
+// console.log( "hello")
+
+// var user = {
+//     name: "User1",
+//     age: 21,
+//     surname: "Surname 2",
+//     friend: {
+//         name: "User2 ",
+//         age: 23
+//     }
+// }
+// console.log(user)
+
+// var user = null
+
+// var mark = undefined
+
+// var isBelieved = false
+
+// console.log(typeof isBelieved)
+
+
+// var sym = Symbol(12)
+// var sym2 = Symbol('a')
+
+// var myAge = "salom"
+// var userAge = "salom"
+// console.log(myAge === userAge)
+
+// var user2 = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 21,
+//     isAdmin: false,
+//     fingerprint: Symbol('John'),
+//     parent: {
+//         firstName: "Aleksey Doe"
+//     }
+// }
+
+// user2.parent.firstName = "Yuliy Doe"
+
+// console.log(user2)
+
+// var fullName = "Bill Gates"
+
+// var fullname = "Mark Zuckerberg"
+
+// console.log(fullName)
+// console.log(fullname)
+// var fullName = prompt("Ismingizni kiriting:", "John Doe")
+
+// console.log("Assalomu alaykum " + fullName)
+
+// alert("Web saytimizga xush kelibsiz?")
+
+// var user = {}
+
+// alert("Assalomu alaykum, Web saytimizga xush kelibsiz?")
+// user.age = Number(prompt("Yoshingizni kiriting:", 20))
+// user.name = prompt("Ism familiyangizni kiriting:", "John Doe")
+
+// console.log(user)
+
+// for, if, switch, else, break, continue, 
+
+var info = {}
+
+alert("Kursga ro'yxatdan o'tish uchun kerakli maydonlarni to'ldiring?")
+info.fullName = prompt('To\'liq ism familiyangini kiriting:', "Falonchiyev ")
+
+info.age = prompt("Yoshingizni kiriting:", 20)
+info.course = prompt("Qaysi yo'nalishga o'qimoqchisiz?", "web dasturlash")
+info.goal = prompt("kursga o'qishdan maqsadingiz?")
+info.reason = prompt("Nega bizning kurslarimizni tanlagansiz?")
+
+console.log("Sizning ma'lumotlarinigiz:", info)
+
+
+
+
+// console.log("Hello\nWorld")
